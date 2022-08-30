9524dd3e0b9be34e434e9f0e0c06f74249c51a1c
Yevhenii <norman78795@gmail.com>
2022-08-30 13:11:26 +0300

    add emloyees data

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,23 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+  const data = [
+    {
+      name: 'Jhon C.',
+      salary: 800,
+      increase: false,
+    }, {
+      name: 'Alex M.',
+      salary: 3000,
+      increase: true,
+    }, {
+      name: 'Carl W.',
+      salary: 5000,
+      increase: false,
+    },
+  ];
+
   return (
     <div className="app">
         <AppInfo />
@@ -16,7 +33,7 @@ function App() {
             <AppFilter/>
         </div>
         
-        <EmployeesList/>
+        <EmployeesList data={data}/>
         <EmployeesAddForm/>
     </div>
   );

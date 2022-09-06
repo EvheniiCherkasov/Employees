36b1dd929a9ecf3f8debd4f94fc9140d22b354f9
Yevhenii <norman78795@gmail.com>
2022-09-06 14:07:57 +0300

    add filter

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,11 +1,30 @@
+import { Component } from 'react';
 import './search-panel.css';
 
-const SearchPanel = () => {
-    return (
+class SearchPanel extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+    }
+
+    onUpdateSearch = (e) => {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term);
+    }
+    
+
+    render() {
+        return (
         <input type="text"
                 className="form-control search-input"
-                placeholder="Найти сотрудника"/>
-    )
+                placeholder="Найти сотрудника"
+                value={this.state.term}
+                onChange={this.onUpdateSearch}/>
+        )
+    }
 }
 
 export default SearchPanel;
